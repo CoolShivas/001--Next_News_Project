fb80d59b7136e6cd5ca075db9a50271fe7f47f6e
CoolShivas <shivas2710cool00@gmail.com>
2024-07-19 00:43:40 +0530

    Creating Dynamic Pages (with Parameters)

---
+++ news_next_project/pages/news/[articles].js
@@ -0,0 +1,14 @@
+
+
+const articles = () => {
+  return (
+    <>
+    <h1> This article is related to the newspaper that has news folder. </h1>
+    <p> This page can be acces by writing in URL below address. </p>
+    <p> After writing the URL /news/write any thing it will route you to this page only. </p>
+    <p> http://localhost:3000/news/anything </p>
+    </>
+  )
+}
+
+export default articles;
